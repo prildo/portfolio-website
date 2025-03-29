03a69c765a8d848086f6165fdfd25fcdff354f5f
dprilmueller <dominik.prilmueller@objectbay.com>
2025-03-29 16:58:23 +0100

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://prildo.github.io',
-  base: '/portfolio-template',
+  base: '/portfolio-website',
   integrations: [tailwind()],
 })
