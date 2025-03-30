3518dcad458c9385af49b150abb4a69f07c8d1fe
dprilmueller <dominik.prilmueller@objectbay.com>
2025-03-30 14:54:37 +0200

    remove astro base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://prildo.github.io',
-  base: '/portfolio-website',
+  // base: '/portfolio-website',
   integrations: [tailwind()],
 })
