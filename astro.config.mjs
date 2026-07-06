9e2f45d685e90535a71d164135ec8078a342bfb8
dprilmueller <dominik.prilmueller@objectbay.com>
2026-07-06 23:42:53 +0200

    Modernize Website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://prildo.github.io',
-  // base: '/portfolio-website',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
